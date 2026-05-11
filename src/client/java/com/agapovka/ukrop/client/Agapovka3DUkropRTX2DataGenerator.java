209c96077c8eaad59a9b33110a9fd3908d5165ca
fomel4ik <fomel4ik.yt@gmail.com>
2026-05-11 21:20:29 +0700

    Initial commit

---
+++ src/client/java/com/agapovka/ukrop/client/Agapovka3DUkropRTX2DataGenerator.java
@@ -0,0 +1,11 @@
+package com.agapovka.ukrop.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Agapovka3DUkropRTX2DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
